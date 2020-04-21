cdad1ec4693890fa1f6518785eebd261ed62c3cc
Nitesh7664 <sharma.nitesh7664@gmail.com>
2020-04-21 22:16:49 +0530

    added resucer method in the state

--- src/context.js
+++ src/context.js
@@ -3,10 +3,23 @@ import axios from 'axios';
 
 const Context = React.createContext();
 
+const reducer = (action, state) => {
+    switch(action.type){
+        case SEARCH_TRACKS:
+            return ({
+                ...state,
+                track_list: action.payload,
+                heading: 'Search Results'
+            });
+        default: return(state);
+    }
+}
+
 export class Provider extends Component {
     state = {
         track_list: [],
-        heading: "Top 10 songs"
+        heading: "Top 10 songs",
+        dispatch: action => this.setState(state => reducer(action, state))
     }
     componentDidMount(){
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=in&f_has_lyrics=1&apikey=c7e9454d8f1738098c2fab1206f980f7`)
