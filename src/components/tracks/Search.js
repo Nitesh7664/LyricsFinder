3c189956cc43976bac25fb48113677469b3c986d
Nitesh7664 <sharma.nitesh7664@gmail.com>
2020-04-21 22:36:44 +0530

    final commit

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -8,16 +8,14 @@ class Search extends Component {
     };
     findTrack = (dispatch, e) => {
         e.preventDefault();
-
+        
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=c7e9454d8f1738098c2fab1206f980f7`)
         .then(res => {
-            this.setState({
-                trackTitle: ""
-            });
             dispatch({
                 type: 'SEARCH_TRACKS',
                 payload: res.data.message.body.track_list
             });
+            this.setState({ trackTitle: "" })
         }).catch(err => {
             console.log(err);
         })
@@ -38,7 +36,7 @@ class Search extends Component {
                         <p className = "lead text-center">Get the lyrics for the song</p>
                         <form onSubmit = {this.findTrack.bind(this, dispatch)}>
                             <div className = "form-group">
-                                <input type = "text" placeholder = "song title..." className = "form-control form-control-lg mb-4" onChange = {this.onChange} name = "trackTitle"/>
+                                <input type = "text" placeholder = "song title..." className = "form-control form-control-lg mb-4" onChange = {this.onChange} value = {this.state.trackTitle} name = "trackTitle"/>
                             </div>
                             <button type = "submit" className = "btn btn-primary btn-block btn-lg mb-4">Search</button>
                         </form>
